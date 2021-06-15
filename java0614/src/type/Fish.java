bc4a3481cdc87c064b4e190eefd64577927eab74
Shiho Kazama(mia) <76821193+miacan2021@users.noreply.github.com>
2021-06-15 09:14:25 -0700

    Update Fish.java

--- java0614/src/type/Fish.java
+++ java0614/src/type/Fish.java
@@ -5,20 +5,18 @@ public class Fish extends Animal{
 
     private String whereLiveIn;
     private String hasGills;
-    private String releaseElectricCharge;
-
 
     public Fish() {
         this.whereLiveIn = "In water";
         this.hasGills = "yes";
-        this.releaseElectricCharge = "can";
+      
     }
 
-    public Fish(String whereLiveIn, String hasGills,  String releaseElectricCharge, int height, int weight, String animalType, String bloodType) {
+    public Fish(String whereLiveIn, String hasGills, int height, int weight, String animalType, String bloodType) {
         super(height, weight, animalType, bloodType);
         this.whereLiveIn = whereLiveIn;
         this.hasGills = hasGills;
-        this.releaseElectricCharge = releaseElectricCharge;
+      
     }
 
     public String getWhereLiveIn() {
@@ -27,12 +25,9 @@ public class Fish extends Animal{
     public String getHasGills() {
         return hasGills;
     }
-    public String getReleaseElectricCharge() {
-        return releaseElectricCharge;
-    }
  
     public String showInfo() {
-        return "Where live in: " + getWhereLiveIn() + " /Has gills?: " + getHasGills() + " /Can release electric charge?: " + getReleaseElectricCharge() +
+        return "Where live in: " + getWhereLiveIn() + " /Has gills?: " + getHasGills() +
         " /Height:" + getHeight() + " /Weight: " + getWeight() + " /AnimalType: " + getAnimalType() + " /BloodType: " + getBloodType() + ".";
     }
 
